d3b4dcdca4b8417a67f84d5237b8e32b44c0765f
Dzakiyaadila <dzakiyaadila4@gmail.com>
2026-05-15 10:18:45 +0700

    add WaitStrategyTest for advance interaction

--- automated-testing/src/test/java/advance_interaction/WaitStrategyTest.java
+++ automated-testing/src/test/java/advance_interaction/WaitStrategyTest.java
@@ -1,58 +1,117 @@
 package advance_interaction;
 
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WaitStrategyTest {
 
     WebDriver driver;
 
-
     @BeforeEach
     void setUp() {
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://practicetestautomation.com/practice-test-exceptions/");
-        driver.manage().timeouts()
-                .implicitlyWait(Duration.ofSeconds(10));
+
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
-//    @Test
-//    void openWebsite() {
-//
-//        Assertions.assertTrue(driver.getTitle().contains("Test Exceptions"));
-//    }
 
     @Test
-    @DisplayName("TC1 - NoSuchElementException")
-    void testCase1Fail() {
+    @DisplayName("TC1 - Failed Without Wait")
+    void testCase1Failed() {
+
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/");
 
         driver.findElement(By.id("add_btn")).click();
         WebElement row2 = driver.findElement(By.id("row2"));
-        Assertions.assertTrue(row2.isDisplayed());
+
+        assertTrue(row2.isDisplayed());
     }
+
     @Test
-    @DisplayName("TC1 - Fixed with Thread.sleep")
+    @DisplayName("TC1-fixed using thread.sleep")
     void testCase1Sleep() throws InterruptedException {
+
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/");
 
         driver.findElement(By.id("add_btn")).click();
 
+        // hard weittt
         Thread.sleep(6000);
-
-        WebElement row2 =
-                driver.findElement(By.id("row2"));
-
-        Assertions.assertTrue(row2.isDisplayed());
+        WebElement row2 = driver.findElement(By.id("row2"));
+        assertTrue(row2.isDisplayed());
     }
 
+    @Test
+    @DisplayName("TC1 - fix pakai explicit wait")
+    void testCase1ExplicitWait() {
+
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/");
+        driver.findElement(By.id("add_btn")).click();
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
+        WebElement row2 = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.id("row2")
+                )
+        );
+
+        assertTrue(row2.isDisplayed());
+    }
+
+    //DI BAWAH INI ADALAH TC 5
+
+    @Test
+    @DisplayName("TC5 - TimeoutException")
+    void testCase5Failed() {
+
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/");
+        driver.findElement(By.id("add_btn")).click();
+
+        // TIMEOUT TOO FAST
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(3));
+
+        WebElement row2 = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.id("row2")
+                )
+        );
+        assertTrue(row2.isDisplayed());
+    }
+
+    @Test
+    @DisplayName("TC5 - Fixed Using Explicit Wait")
+    void testCase5Fixed() {
+
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/");
+        driver.findElement(By.id("add_btn")).click();
+
+        // Timeout diperpanjang
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement row2 = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("row2")
+                )
+        );
+
+        assertTrue(row2.isDisplayed());
+    }
 
     @AfterEach
     void tearDown() {
 
 //        driver.quit();
+
     }
 }
